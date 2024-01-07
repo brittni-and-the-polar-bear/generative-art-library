7feee0d570228c4aa55932a7c7fb97000833005d
blwatkins <blwatkins@users.noreply.github.com>
2024-01-07 12:04:16 -0600

    #1 Add initial files for p5 exports and sketch-context.ts

---
+++ src/p5/sketch-context.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2024 Brittni Watkins.
+ *
+ * This file is a part of brittni and the polar bear's Generative Art Library,
+ * which is released under the GNU Affero General Public License, Version 3.0.
+ * You may not use this file except in compliance with the license.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. See LICENSE or go to
+ * https://www.gnu.org/licenses/agpl-3.0.en.html for full license details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ */
+
