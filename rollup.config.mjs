3c89bd3d31ad952e0f9f1bdbc90fb99d8cf20f46
azurepolarbear <azurepolarbear@users.noreply.github.com>
2024-02-17 22:52:55 -0600

    Update package to be built with Rollup.

---
+++ rollup.config.mjs
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2024 brittni and the polar bear LLC.
+ *
+ * This file is a part of brittni and the polar bear's Generative Art Library,
+ * which is released under the GNU Affero General Public License, Version 3.0.
+ * You may not use this file except in compliance with the license.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. See LICENSE or go to
+ * https://www.gnu.org/licenses/agpl-3.0.en.html for full license details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ */
+
+import eslint from '@rollup/plugin-eslint';
+import terser from '@rollup/plugin-terser';
+
+import analyzer from 'rollup-plugin-analyzer';
+import ts from 'rollup-plugin-ts';
+
+export default {
+    input: './src/main/index.ts',
+    output: {
+        dir: './out',
+        format: 'umd',
+        name: 'genart',
+        sourcemap: true,
+        preserveModules: false,
+        globals: {
+            '@batpb/genart-base': 'genartBase',
+            '@batpb/genart-palettes': 'genartPalettes'
+        }
+    },
+    external: ['@batpb/genart-base', '@batpb/genart-palettes'],
+    plugins: [
+        eslint({
+            include: ['./src/**/*.ts'],
+            throwOnError: true,
+            throwOnWarning: true
+        }),
+        terser(),
+        analyzer({
+            summaryOnly: true
+        }),
+        ts()
+    ]
+};
