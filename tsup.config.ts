21b358bbc1821ff023048a4efde5855e0f126122
blwatkins <blwatkins@users.noreply.github.com>
2024-08-01 13:37:16 -0500

    #135 add tsup configuration. tsup replacing rollup.

---
+++ tsup.config.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2024 brittni and the polar bear LLC.
+ *
+ * This file is a part of brittni and the polar bear's Generative Art Library,
+ * which is released under the GNU Affero General Public License, Version 3.0.
+ * You may not use this file except in compliance with the license.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. See LICENSE or go to
+ * https://www.gnu.org/licenses/agpl-3.0.en.html for full license details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ */
+
+// TODO - test docs
+// TODO - update npm github actions to include lint
+// TODO - add changelog library (new issue?)
+// TODO - rerun tests
+
+import {defineConfig} from 'tsup'
+
+export default defineConfig({
+    entry: ['src/main/batpb-genart.ts'],
+    outDir: './out/dist',
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    dts: true,
+    format: ['cjs', 'esm'],
+    minifySyntax: true
+});
