bc5c457fc7798bc2ecc36473e7a8b41aec2b5c59
blwatkins <blwatkins@users.noreply.github.com>
2024-08-04 14:16:27 -0500

    Update documentation for next release.

--- docs/doc/assets/icons.js
+++ docs/doc/assets/icons.js
@@ -1,6 +1,5 @@
-(function () {
+(function() {
     addIcons();
-
     function addIcons() {
         if (document.readyState === "loading") return document.addEventListener("DOMContentLoaded", addIcons);
         const svg = document.body.appendChild(document.createElementNS("http://www.w3.org/2000/svg", "svg"));
