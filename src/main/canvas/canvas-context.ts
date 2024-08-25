c77a9b963e18459fa4eb86488bbf38508e146a04
blwatkins <blwatkins@users.noreply.github.com>
2024-08-24 21:07:56 -0500

    #61 add methods for min height, max height, min width, max width, and default stroke.

--- src/main/canvas/canvas-context.ts
+++ src/main/canvas/canvas-context.ts
@@ -15,6 +15,10 @@
  * See the GNU Affero General Public License for more details.
  */
 
+import P5Lib from 'p5';
+
+import { SketchContext } from 'context';
+
 /**
  * The CanvasContext provides static access to information and methods
  * for the running p5.js sketch canvas.
@@ -32,5 +36,65 @@ export class CanvasContext {
      */
     public static get isWebGL(): boolean {
         return CanvasContext._isWebGL;
+    }
+
+    // TODO - docs
+    // TODO - unit test
+    public static get maxHeight(): number {
+        const p5: P5Lib = SketchContext.p5;
+        let max: number = p5.height;
+
+        if (CanvasContext.isWebGL) {
+            max = (p5.height / 2.0);
+        }
+
+        return max;
+    }
+
+    // TODO - docs
+    // TODO - unit tests
+    public static get maxWidth(): number {
+        const p5: P5Lib = SketchContext.p5;
+        let max: number = p5.width;
+
+        if (CanvasContext.isWebGL) {
+            max = (p5.width / 2.0);
+        }
+
+        return max;
+    }
+
+    // TODO - docs
+    // TODO - unit tests
+    public static get minHeight(): number {
+        const p5: P5Lib = SketchContext.p5;
+        let min: number = 0;
+
+        if (CanvasContext.isWebGL) {
+            min = (p5.height / 2.0) * -1.0;
+        }
+
+        return min;
+    }
+
+    // TODO - docs
+    // TODO - unit tests
+    public static get minWidth(): number {
+        const p5: P5Lib = SketchContext.p5;
+        let min: number = 0;
+
+        if (CanvasContext.isWebGL) {
+            min = (p5.width / 2.0) * -1.0;
+        }
+
+        return min;
+    }
+
+    // TODO - unit test
+    // TODO - docs
+    public static get defaultStroke(): number {
+        const { p5 } = SketchContext;
+        const maxDimension: number = Math.max(p5.width, p5.height);
+        return maxDimension * 0.002;
     }
 }
