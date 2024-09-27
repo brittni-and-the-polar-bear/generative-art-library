bc808e1c82bef51c31a9d9186c2858368d2b6adc
blwatkins <blwatkins@users.noreply.github.com>
2024-09-26 21:14:27 -0500

    #61 Add width and height tests for aspect ratio handler.

--- src/test/canvas/aspect-ratio/aspect-ratio-handler.test.ts
+++ src/test/canvas/aspect-ratio/aspect-ratio-handler.test.ts
@@ -22,9 +22,12 @@ describe('AspectRatioHandler tests', (): void => {
         const expectedWidth: number = 3;
         const expectedHeight: number = 5;
         const expectedName: string = 'test ratio';
-        const aspectRatio: AspectRatio | undefined = AspectRatioHandler.buildAspectRatio(expectedWidth,
-                                                                                         expectedHeight,
-                                                                                         expectedName);
+        const aspectRatio: AspectRatio | undefined =
+            AspectRatioHandler.buildAspectRatio(
+                expectedWidth,
+                expectedHeight,
+                expectedName
+            );
         expect(aspectRatio).toBeTruthy();
         expect(aspectRatio?.WIDTH_RATIO).toBe(expectedWidth);
         expect(aspectRatio?.HEIGHT_RATIO).toBe(expectedHeight);
@@ -49,23 +52,9 @@ describe('AspectRatioHandler tests', (): void => {
         expect(aspectRatio).toBeUndefined();
     });
 
-    test('AspectRatioHandler.buildAspectRatio - negative width ratio', (): void => {
-        const width: number = -3;
-        const height: number = 3;
-        const aspectRatio: AspectRatio | undefined = AspectRatioHandler.buildAspectRatio(width, height);
-        expect(aspectRatio).toBeUndefined();
-    });
-
     test('AspectRatioHandler.buildAspectRatio - height ratio < 1', (): void => {
         const width: number = 3;
         const height: number = 0.9999;
-        const aspectRatio: AspectRatio | undefined = AspectRatioHandler.buildAspectRatio(width, height);
-        expect(aspectRatio).toBeUndefined();
-    });
-
-    test('AspectRatioHandler.buildAspectRatio - negative height ratio', (): void => {
-        const width: number = 5;
-        const height: number = -2;
         const aspectRatio: AspectRatio | undefined = AspectRatioHandler.buildAspectRatio(width, height);
         expect(aspectRatio).toBeUndefined();
     });
@@ -77,10 +66,87 @@ describe('AspectRatioHandler tests', (): void => {
         expect(aspectRatio).toBeUndefined();
     });
 
-    test('AspectRatioHandler.buildAspectRatio - negative width and height ratios', (): void => {
-        const width: number = -1.3;
-        const height: number = -4.2;
-        const aspectRatio: AspectRatio | undefined = AspectRatioHandler.buildAspectRatio(width, height);
-        expect(aspectRatio).toBeUndefined();
+    test.each(
+        [
+            { widthRatio: 1, heightRatio: 1, resolution: 720, expectedWidth: 720, expectedHeight: 720 },
+            { widthRatio: 1, heightRatio: 1.5, resolution: 720, expectedWidth: 720, expectedHeight: 1080 },
+            { widthRatio: 1.5, heightRatio: 1, resolution: 720, expectedWidth: 1080, expectedHeight: 720 },
+            { widthRatio: 1, heightRatio: 2, resolution: 720, expectedWidth: 720, expectedHeight: 1440 },
+            { widthRatio: 2, heightRatio: 1, resolution: 720, expectedWidth: 1440, expectedHeight: 720 },
+            { widthRatio: 3, heightRatio: 4, resolution: 720, expectedWidth: 720, expectedHeight: 960 },
+            { widthRatio: 4, heightRatio: 3, resolution: 720, expectedWidth: 960, expectedHeight: 720 },
+            { widthRatio: 7, heightRatio: 10, resolution: 720, expectedWidth: 720, expectedHeight: 1028 },
+            { widthRatio: 10, heightRatio: 7, resolution: 720, expectedWidth: 1028, expectedHeight: 720 },
+            { widthRatio: 1, heightRatio: 1, resolution: 1080, expectedWidth: 1080, expectedHeight: 1080 },
+            { widthRatio: 1, heightRatio: 1.5, resolution: 1080, expectedWidth: 1080, expectedHeight: 1620 },
+            { widthRatio: 1.5, heightRatio: 1, resolution: 1080, expectedWidth: 1620, expectedHeight: 1080 },
+            { widthRatio: 1, heightRatio: 2, resolution: 1080, expectedWidth: 1080, expectedHeight: 2160 },
+            { widthRatio: 2, heightRatio: 1, resolution: 1080, expectedWidth: 2160, expectedHeight: 1080 },
+            { widthRatio: 3, heightRatio: 4, resolution: 1080, expectedWidth: 1080, expectedHeight: 1440 },
+            { widthRatio: 4, heightRatio: 3, resolution: 1080, expectedWidth: 1440, expectedHeight: 1080 },
+            { widthRatio: 7, heightRatio: 10, resolution: 1080, expectedWidth: 1080, expectedHeight: 1542 },
+            { widthRatio: 10, heightRatio: 7, resolution: 1080, expectedWidth: 1542, expectedHeight: 1080 }
+        ]
+    )('width and height properties: $widthRatio:$heightRatio; $resolution',
+        ({ widthRatio, heightRatio, resolution, expectedWidth, expectedHeight }): void => {
+            const aspectRatio: AspectRatio | undefined = AspectRatioHandler.buildAspectRatio(widthRatio, heightRatio);
+            expect(aspectRatio).toBeTruthy();
+
+            if (aspectRatio) {
+                const handler: AspectRatioHandler = new AspectRatioHandler(aspectRatio, resolution);
+                expect(handler.width).toBe(expectedWidth);
+                expect(handler.height).toBe(expectedHeight);
+            }
+        }
+    );
+
+    test('width and height properties - zero resolution', (): void => {
+        const width: number = 3;
+        const height: number = 21;
+        const resolution: number = 0;
+        const aspectRatio: AspectRatio = { WIDTH_RATIO: width, HEIGHT_RATIO: height, NAME: 'test ratio' };
+        const handler: AspectRatioHandler = new AspectRatioHandler(aspectRatio, resolution);
+        expect(handler.width).toBe(0);
+        expect(handler.height).toBe(0);
+    });
+
+    test('width and height properties - negative resolution', (): void => {
+        const width: number = 7;
+        const height: number = 9;
+        const resolution: number = -1080;
+        const aspectRatio: AspectRatio = { WIDTH_RATIO: width, HEIGHT_RATIO: height, NAME: 'test ratio' };
+        const handler: AspectRatioHandler = new AspectRatioHandler(aspectRatio, resolution);
+        expect(handler.width).toBe(0);
+        expect(handler.height).toBe(0);
+    });
+
+    test('width and height properties - width ratio < 1', (): void => {
+        const width: number = 0.993;
+        const height: number = 2;
+        const resolution: number = 720;
+        const aspectRatio: AspectRatio = { WIDTH_RATIO: width, HEIGHT_RATIO: height, NAME: 'test ratio' };
+        const handler: AspectRatioHandler = new AspectRatioHandler(aspectRatio, resolution);
+        expect(handler.width).toBe(0);
+        expect(handler.height).toBe(0);
+    });
+
+    test('width and height properties - height ratio < 1', (): void => {
+        const width: number = 3.2;
+        const height: number = 0.111;
+        const resolution: number = 720;
+        const aspectRatio: AspectRatio = { WIDTH_RATIO: width, HEIGHT_RATIO: height, NAME: 'test ratio' };
+        const handler: AspectRatioHandler = new AspectRatioHandler(aspectRatio, resolution);
+        expect(handler.width).toBe(0);
+        expect(handler.height).toBe(0);
+    });
+
+    test('width and height properties - width and height ratios < 1', (): void => {
+        const width: number = 0.2;
+        const height: number = 0.333;
+        const resolution: number = 720;
+        const aspectRatio: AspectRatio = { WIDTH_RATIO: width, HEIGHT_RATIO: height, NAME: 'test ratio' };
+        const handler: AspectRatioHandler = new AspectRatioHandler(aspectRatio, resolution);
+        expect(handler.width).toBe(0);
+        expect(handler.height).toBe(0);
     });
 });
