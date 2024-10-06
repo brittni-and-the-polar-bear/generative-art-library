3dcb648399da91f18a91e86f4d8f38c226b222c1
blwatkins <blwatkins@users.noreply.github.com>
2024-10-06 09:31:05 -0500

    Add new file for KeyboardMapper class.

---
+++ src/main/p5/keyboard/keyboard-mapper.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2024 brittni and the polar bear LLC.
+ *
+ * This file is a part of brittni and the polar bear's generative art library,
+ * which is released under the GNU Affero General Public License, Version 3.0.
+ * You may not use this file except in compliance with the license.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. See LICENSE or go to
+ * https://www.gnu.org/licenses/agpl-3.0.en.html for full license details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ */
+
+// TODO - rename p5 directory to sketch
+// TODO - rename sketch-context to p5-context (put in p5 directory)
+// TODO - move canvas directory to sketch directory
+// TODO - export everything from sketch-context namespace
+
+// NOTE: static functions
+export class KeyboardMapper {
+    // TODO - map character to void function
+    // TODO - map keycode to void function
+    // TODO - capture key strokes to execute mapped functions
+    // TODO - lock mappings so they cannot be replaced
+    // TODO - method to set multiple mappings at once
+    // TODO - KeyMapping object/type/interface structure - (key/keycode, function, isLocked)
+    // TODO - Retrieve mappings for mobile UI
+}
