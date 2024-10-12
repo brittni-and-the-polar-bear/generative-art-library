f7f0ef0be57ec0432e85d3269c1cfdb5894e8175
blwatkins <blwatkins@users.noreply.github.com>
2024-10-12 08:01:52 -0500

    Add run of show list to see TODO items in order in IDE

---
+++ src/main/run-of-show.ts
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2024 brittni and the polar bear LLC.
+ *
+ * This file is a part of brittni and the polar bear's generative art library,
+ * which is released under the GNU Affero General Public License, Version 3.0.
+ * You may not use this file except in compliance with the license.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. See LICENSE or go to
+ * https://www.gnu.org/licenses/agpl-3.0.en.html for full license details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ */
+
+// TODO - initial aspect ratio matching
+// TODO - dynamic window matching aspect ratio
+// TODO - complete issue #61: create canvas context class
+// TODO - new palette
+// TODO - next version release
+
+// TODO - issue #91: string validator
+// TODO - issue #119: Add method to ColorNameManager to Add a Name Based on a Hex Value
+// TODO - issue #139 progress: Review rules in eslint plugin - suggestion rules
+// TODO - new palette
+// TODO - next version release
+
+// TODO - issue #139 progress: Review rules in eslint plugin - layout and formatting rules
+// TODO - lerp color functions
+// TODO - new palette
+// TODO - next version release
+
+// TODO - issue #137: Review rules in stylistic eslint plugin
+// TODO - rgb colors
+// TODO - new palette
+// TODO - next version release
+
+// TODO - hsl colors
+// TODO - new palette
+// TODO - next version release
+
+// TODO - fxhash
+// TODO - new palette
+// TODO - next version release
+
+// TODO - issue #161: keyboard mapper
+// TODO - new palette
+// TODO - next version release
+
+// TODO - issue #165: Mouse Click Mapper
+// TODO - new palette
+// TODO - next version release
+
+// TODO - issue #166: Mouse Hover Mapper
+// TODO - new palette
+// TODO - next version release
+
+// TODO - mobile interaction
+// TODO - new palette
+// TODO - next version release
+
+// TODO - issue #130: Make Jekyll Collection for Palette Colors
+// TODO - issue #131: Make Jekyll Collection for Palettes
+// TODO - issue #132: Make Jekyll Collection for Release Notes
+// TODO - issue #160: Page with All Colors in GitHub Pages
+// TODO - palette explorer
+// TODO - issue #151: random palette builder
+// TODO - new palette
+// TODO - next version release
+
+// TODO - issue #76: MappableColorSelector Interface
+// TODO - issue #77: OrderedPaletteColorSelector Subclass
+// TODO - new palette
+// TODO - next version release
+
+// TODO - issue #96: High Contrast Palette Color Selector
+// TODO - new palette
+// TODO - next version release
+
+// TODO - issue #126: Weighted Palettes
+// TODO - new palette
+// TODO - next version release
+
+// TODO - issue #80: PaletteColorSelector Factory
+// TODO - new palette
+// TODO - next version release
+
+// TODO - issue #62: Weighted ColorSelectorManager
+// TODO - new palette
+// TODO - next version release
