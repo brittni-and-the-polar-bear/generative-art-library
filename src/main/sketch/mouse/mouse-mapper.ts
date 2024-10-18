a8d8ca57170ae15af8bdb7b0c045cbe320637677
blwatkins <blwatkins@users.noreply.github.com>
2024-10-17 21:29:20 -0500

    #61 Add documentation to MouseMapper methods

--- src/main/sketch/mouse/mouse-mapper.ts
+++ src/main/sketch/mouse/mouse-mapper.ts
@@ -18,39 +18,59 @@
 import { P5Context } from '../p5-context';
 import { CoordinateMapper } from '../canvas';
 
-// TODO - documentation
 // TODO - release notes
 /**
+ * The MouseMapper will map the current position of the mouse to
+ * corresponding locations on the canvas.
+ *
  * @category Sketch Context
  * @category Sketch Context: Mouse
  */
 export class MouseMapper {
-    // TODO - documentation
     // TODO - release notes
+    /**
+     * The current mouseX position.
+     */
     public static get mouseX(): number {
         return MouseMapper.mapMouseXToCanvas(P5Context.p5.mouseX);
     }
 
-    // TODO - documentation
     // TODO - release notes
+    /**
+     * The current mouseY position.
+     */
     public static get mouseY(): number {
         return MouseMapper.mapMouseYToCanvas(P5Context.p5.mouseY);
     }
 
-    // TODO - documentation
     // TODO - release notes
+    /**
+     * The current ratio value of the mouseX position,
+     * where the left edge of the canvas maps to 0,
+     * and the right edge of the canvas maps to 1.
+     */
     public static get mouseXRatio(): number {
         return MouseMapper.mapMouseXToRatio(P5Context.p5.mouseX);
     }
 
-    // TODO - documentation
     // TODO - release notes
+    /**
+     * The current ratio value of the mouseY position,
+     * where the top edge of the canvas maps to 0,
+     * and the bottom edge of the canvas maps to 1.
+     */
     public static get mouseYRatio(): number {
         return MouseMapper.mapMouseYToRatio(P5Context.p5.mouseY);
     }
 
-    // TODO - documentation
     // TODO - release notes
+    /**
+     * Map the provided x-axis mouse position to the corresponding value
+     * on the scale of {@link CoordinateMapper.minX} to
+     * {@link CoordinateMapper.maxX}.
+     *
+     * @param mouseX
+     */
     public static mapMouseXToCanvas(mouseX: number): number {
         const p5: P5Lib = P5Context.p5;
         return p5.map(mouseX, 0, p5.width, CoordinateMapper.minX, CoordinateMapper.maxX);
