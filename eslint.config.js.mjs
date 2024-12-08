1dcd8c9908de283470ade1413acb021a701c1023
blwatkins <blwatkins@users.noreply.github.com>
2024-12-08 17:09:17 -0600

    Add configuration to eslint.config.js.mjs

---
+++ eslint.config.js.mjs
@@ -0,0 +1,153 @@
+/*
+ * Copyright (C) 2024 brittni and the polar bear LLC.
+ *
+ * This file is a part of brittni and the polar bear's generative art library,
+ * which is released under the GNU Affero General Public License, Version 3.0.
+ * You may not use this file except in compliance with the license.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. See LICENSE or go to
+ * https://www.gnu.org/licenses/agpl-3.0.en.html for full license details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ */
+
+// This configuration is designed to parse all JavaScript configuration files
+
+import eslint from '@eslint/js';
+
+import es_x from 'eslint-plugin-es-x';
+import node from 'eslint-plugin-n';
+import security from 'eslint-plugin-security';
+
+import stylistic from '@stylistic/eslint-plugin'
+
+export default [
+    eslint.configs.recommended,
+    es_x.configs['flat/restrict-to-es2022'],
+    node.configs["flat/recommended"],
+    security.configs.recommended,
+    stylistic.configs['recommended-flat'],
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module'
+        },
+        rules: {
+            /* @eslint/js */
+            'array-callback-return': ['error', {
+                checkForEach: true
+            }],
+
+            'no-await-in-loop': 'error',
+
+            'no-cond-assign': ['error', 'always'],
+
+            'no-constant-condition': ['error', {
+                checkLoops: 'all'
+            }],
+
+            'no-constructor-return': 'error',
+
+            'no-duplicate-imports': ['error', {
+                includeExports: true
+            }],
+
+            'no-inner-declarations': ['error', 'both'],
+
+            'no-promise-executor-return': 'error',
+
+            'no-self-compare': 'error',
+
+            'no-template-curly-in-string': 'error',
+
+            'no-unmodified-loop-condition': 'error',
+
+            'no-unreachable-loop': 'error',
+
+            'no-unsafe-negation': ['error', {
+                enforceForOrderingRelations: true
+            }],
+
+            'no-unsafe-optional-chaining': ['error', {
+                disallowArithmeticOperators: true
+            }],
+
+            'no-useless-assignment': 'error',
+
+            'require-atomic-updates': 'error',
+
+            'use-isnan': ['error', {
+                enforceForSwitchCase: true,
+                enforceForIndexOf: true
+            }],
+
+            'valid-typeof': ['error', {
+                requireStringLiterals: true
+            }],
+
+            'one-var': ['error', 'never'],
+
+            /* @stylistic/eslint-plugin */
+
+            '@stylistic/brace-style': ['error', '1tbs'],
+
+            '@stylistic/no-extra-semi': 'error',
+
+            '@stylistic/function-call-argument-newline': ['error', 'consistent'],
+
+            '@stylistic/function-call-spacing': ['error', 'never'],
+
+            '@stylistic/comma-dangle': ['error', 'never'],
+
+            '@stylistic/indent': ['error',
+                4,
+                {
+                    SwitchCase: 1,
+                    FunctionDeclaration: {
+                        parameters: 'first'
+                    },
+                    FunctionExpression: {
+                        parameters: 'first'
+                    }
+                }
+            ],
+
+            '@stylistic/indent-binary-ops': 'off',
+
+            '@stylistic/member-delimiter-style': ['error', {
+                multiline: {
+                    delimiter: 'semi',
+                    requireLast: true
+                },
+                singleline: {
+                    delimiter: 'semi',
+                    requireLast: true
+                }
+            }],
+
+            '@stylistic/operator-linebreak': ['error', 'after'],
+
+            '@stylistic/quotes': ['error',
+                'single',
+                {
+                    avoidEscape: true,
+                    allowTemplateLiterals: true
+                }
+            ],
+
+            '@stylistic/semi': ['error', 'always'],
+
+            /* eslint-plugin-security */
+
+            'security/detect-object-injection': 'off',
+
+            /* eslint-plugin-n */
+
+            'n/no-missing-import': 'off',
+        }
+    }
+];
