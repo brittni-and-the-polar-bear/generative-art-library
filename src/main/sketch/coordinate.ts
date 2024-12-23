fd5bf46dd8fcd9a3c8e4a4a670df26be345048c2
blwatkins <blwatkins@users.noreply.github.com>
2024-12-22 21:27:42 -0600

    Add new coordinate file.

---
+++ src/main/sketch/coordinate.ts
@@ -0,0 +1,11 @@
+
+// TODO - Coordinate constructor should be able to accept:
+// TODO -  - no arguments
+// TODO -  - (x, y) coordinate pair
+// TODO -  - (x, y, z) coordinate pair
+// TODO -  - another Coordinate object
+// TODO -  - P5Lib.Vector object
+
+export class Coordinate {
+
+}
